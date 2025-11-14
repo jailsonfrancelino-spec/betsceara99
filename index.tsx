36802c85efae3966bd931dd355abb1e764e32a8c
jailsonfrancelino-spec <jailsonfrancelino@hotmail.com>
2025-11-14 15:20:26 -0300

    feat: Add Firebase integration
    Integrates Firebase for backend services, including Firestore for data storage and Analytics.
    This change adds Firebase to the project dependencies and initializes it in the main application entry point.

--- index.tsx
+++ index.tsx
@@ -1,7 +1,30 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBSkoeMMJAGaHKDGjkVKbP45rSExs5OEdM",
+  authDomain: "betsceara99app.firebaseapp.com",
+  projectId: "betsceara99app",
+  storageBucket: "betsceara99app.firebasestorage.app",
+  messagingSenderId: "871863085444",
+  appId: "1:871863085444:web:07baef933a782decd315d1",
+  measurementId: "G-C1C8MG0LKR"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const db = getFirestore(app);
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
